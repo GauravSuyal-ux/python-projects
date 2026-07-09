d53bf67a919753f841c59bf69289589bee9f686d
GauravSuyal-ux <suyalgaurav98@gmail.com>
2026-07-09 20:28:37 +0530

    Add range validation for user guesses

--- 02_Guessing_Game/guessing_game.py
+++ 02_Guessing_Game/guessing_game.py
@@ -8,15 +8,21 @@ while True:
     try:
         guess=int(input("Guess the number: "))
     except ValueError:
-        print("Invalid Input! Enter a integer number between 1-100")
+        print("Invalid input! Please enter a valid integer.")
         continue
-
-    if guess==secret_num:
-        print(f"Yes! that's correct, the number is {secret_num}..",f"You guessed in {count} attempts.")
-        break
-    elif guess>secret_num:
-        print("You guessed the grater one,Try to think the smaller one.. ")
-    else:
-        print("You guessed the smaller one, Try to think grater number..")
     
-    count+=1
+    if guess>=1 and guess<=100:
+        
+        if guess==secret_num:
+            print(f"Yes! that's correct, the number is {secret_num}..",f"You guessed in {count} attempts.")
+            break
+        elif guess>secret_num:
+            print("You guessed the grater one,Try to think the smaller one.. ")
+        else:
+            print("You guessed the smaller one, Try to think grater number..")
+    
+        count+=1
+
+    else:
+        print("Please enter a number between 1 and 100.")
+        continue
