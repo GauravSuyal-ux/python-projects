3feb16e4a5e488f08cf228660deebd37fa45c8b5
GauravSuyal-ux <suyalgaurav98@gmail.com>
2026-07-09 22:52:33 +0530

    Add play again feature to guessing game

--- 02_Guessing_Game/guessing_game.py
+++ 02_Guessing_Game/guessing_game.py
@@ -1,28 +1,44 @@
 import random
-secret_num = random.randint(1,100)
-print(secret_num)
-count = 1
-print("======Guess the number between 1-100======")
 while True:
-   # guess = int(input("Guess the number: "))
-    try:
-        guess=int(input("Guess the number: "))
-    except ValueError:
-        print("Invalid input! Please enter a valid integer.")
-        continue
-    
-    if guess>=1 and guess<=100:
-        
-        if guess==secret_num:
-            print(f"Yes! that's correct, the number is {secret_num}..",f"You guessed in {count} attempts.")
-            break
-        elif guess>secret_num:
-            print("You guessed the grater one,Try to think the smaller one.. ")
-        else:
-            print("You guessed the smaller one, Try to think grater number..")
-    
-        count+=1
+    secret_num = random.randint(1, 100)
+    count = 1
 
-    else:
-        print("Please enter a number between 1 and 100.")
-        continue
+    print("====== Guess the Number Between 1-100 ======")
+
+    while True:
+        try:
+            guess = int(input("Guess the number: "))
+
+        except ValueError:
+            print("Invalid input! Enter an integer between 1 and 100.")
+            continue
+
+        if 1 <= guess <= 100:
+
+            if guess == secret_num:
+                print(f"Yes! That's correct. The number is {secret_num}.")
+                print(f"You guessed it in {count} attempts.")
+                break
+
+            elif guess > secret_num:
+                print("You guessed a greater number. Try a smaller one.")
+                count += 1
+
+            else:
+                print("You guessed a smaller number. Try a greater one.")
+                count += 1
+
+        else:
+            print("Please enter a number between 1 and 100.")
+            continue
+    
+    while True:
+        choice= input("Play again?? (y/n)").lower()
+
+        if choice == "y":
+            break
+        elif choice=="n":
+            print("Thanks for playing!...") 
+            exit()
+        else:
+            print("Please enter a valid choice 'y' or 'n':")
