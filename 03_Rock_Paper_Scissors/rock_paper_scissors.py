ffc4ba53837ac36aa7bc24c92743ed682269eea0
GauravSuyal-ux <suyalgaurav98@gmail.com>
2026-07-10 23:42:51 +0530

    Implement winner decision logic

--- 03_Rock_Paper_Scissors/rock_paper_scissors.py
+++ 03_Rock_Paper_Scissors/rock_paper_scissors.py
@@ -5,6 +5,7 @@ def get_computer_choice():
     choices=["rock","paper","scissors"]
     return(random.choice(choices))
 
+
 def get_user_choice():
     while True:
         user_choice=input("Enter your choice (rock, paper, or scissors): ").strip().lower()
@@ -13,5 +14,20 @@ def get_user_choice():
         else:
             print("Invalid choice! Please enter rock, paper, or scissors.")
 
-get_computer_choice()
+
+def decide_winner(user_choice, computer_choice):
+    if user_choice == computer_choice:
+        return "Draw"
+    
+    elif(
+        (user_choice=="rock" and computer_choice=="scissors")
+        or 
+        (user_choice=="scissors" and computer_choice=="paper" )
+        or
+        (user_choice=="paper" and computer_choice=="rock")
+    ):
+        return "You Win"
+
+    else:
+        return"Computer Wins"
    
