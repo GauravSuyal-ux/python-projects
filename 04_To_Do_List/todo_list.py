ec5f3d937370ac94b6d978603dd77bae2f0e057a
GauravSuyal-ux <suyalgaurav98@gmail.com>
2026-07-12 00:55:41 +0530

    Implement menu system with input validation

--- 04_To_Do_List/todo_list.py
+++ 04_To_Do_List/todo_list.py
@@ -1,3 +1,12 @@
 todo_list=[]
 while True:
-    
+    print ("==== Terminal Based To Do List ====")
+    print("Menu-\n1.Add Task\n2.View Tasks\n3.Delete Task\n4.Exit")
+
+    try:
+        user_choice=int(input("Enter your choice (1-4):"))
+    except ValueError:
+        print("Invalid Choice! Please enter a number")
+        continue
+    if user_choice not in (1,5):
+        print("Please enter your choice in (1-4)")
