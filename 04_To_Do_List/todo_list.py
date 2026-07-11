32fe9746b867c8166a7348d28166bd8ecca8d3a9
GauravSuyal-ux <suyalgaurav98@gmail.com>
2026-07-12 01:39:52 +0530

    Implement add task functionality

--- 04_To_Do_List/todo_list.py
+++ 04_To_Do_List/todo_list.py
@@ -1,4 +1,5 @@
 todo_list=[]
+
 while True:
     print ("==== Terminal Based To Do List ====")
     print("Menu-\n1.Add Task\n2.View Tasks\n3.Delete Task\n4.Exit")
@@ -8,5 +9,9 @@ while True:
     except ValueError:
         print("Invalid Choice! Please enter a number")
         continue
-    if user_choice not in (1,5):
-        print("Please enter your choice in (1-4)")
+    if user_choice==1:
+        task=input("Please enter your task:").strip()
+        todo_list.append(task)
+        print("Task added Successfully")
+    
+                
