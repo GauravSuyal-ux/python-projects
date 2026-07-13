89683d6aaa9f8409a490f44cabec8159d5850a1e
GauravSuyal-ux <suyalgaurav98@gmail.com>
2026-07-14 00:07:04 +0530

    Add delete task functionality with input validation

--- 04_To_Do_List/todo_list.py
+++ 04_To_Do_List/todo_list.py
@@ -9,10 +9,13 @@ while True:
     except ValueError:
         print("Invalid Choice! Please enter a number")
         continue
+
     if user_choice==1:
         task=input("Please enter your task:").strip()
         todo_list.append(task)
         print("Task added Successfully")
+
+
     elif user_choice==2:
         if len(todo_list)==0:
             print("No Tasks Available.")
@@ -22,3 +25,22 @@ while True:
                 print(f"{counter}.{task}")
                 counter+=1
                 
+    elif user_choice==3:
+         while True:
+            if len(todo_list)==0:
+                print("Sorry! No tasks available")
+                break
+        
+            else:
+                try:
+                    user_input=int(input(f"Enter the no. of task from 1-{len(todo_list)}which you want delete: "))
+                except ValueError:
+                    print("Please enter a valid integer:")
+                    continue
+            
+                if 1 <= user_input <= len(todo_list):
+                    removed_task=todo_list.pop(user_input-1)
+                    print(f"Task '{removed_task}' deleted successfully")
+                    break
+                else:
+                    print(f"Please enter the number which is >=1 and <={len(todo_list)}")
